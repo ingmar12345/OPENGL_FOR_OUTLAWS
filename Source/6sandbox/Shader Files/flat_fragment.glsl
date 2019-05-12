63fa83878e8a923cbb23de81d3b569b737b3c32a
ingmar12345 <ingmarkroon@gmail.com>
2019-05-12 23:16:43 +0200

    Create flat_fragment.glsl

---
+++ Source/6sandbox/Shader Files/flat_fragment.glsl
@@ -0,0 +1,37 @@
+#version 420 core
+
+in vec3 geoPosWorld;
+in vec3 geoNormWorld;
+out vec4 color;
+
+uniform vec3 modelColor;
+uniform float modelShine;
+uniform vec3 lightPos;
+uniform vec3 viewPos;
+
+
+void main(void)
+{
+	// ambient component
+	float ambientFactor = 0.3f;
+	vec3 ambient = ambientFactor * modelColor;
+
+	// diffuse component
+	vec3 lightVec = normalize(lightPos - geoPosWorld);
+	vec3 normVec = normalize(geoNormWorld);
+	float diffuseFactor = max(dot(normVec, lightVec), 0.0f);
+	vec3 diffuse = diffuseFactor * modelColor;
+
+	// specular component
+	vec3 viewVec = normalize(viewPos - geoPosWorld);
+	vec3 reflectVec = reflect(-lightVec, normVec);
+	float specFactor = pow(max(dot(viewVec, reflectVec), 0.0f), 16);
+	float specShine = modelShine;
+	vec3 specColor = vec3(1.0f, 1.0f, 1.0f); 
+	vec3 specular = specShine * specFactor * specColor;
+
+	// resultaat
+	vec3 result = ambient + diffuse + specular;
+
+	color = vec4(result, 1.0f);
+}
